c7d96f85ab8f3109eef425f7afa60ca7b301e530
bartoszzak <62252217+bartoszzak@users.noreply.github.com>
2021-12-07 23:11:50 +0100

    wstepnie algorytm

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,6 @@
 from solution import *
 import random
+from copy import deepcopy
 
 
 def create_initial_population(population_size: int, event_list: List[Event], distances: Dict, start_date: datetime,
@@ -7,8 +8,8 @@ def create_initial_population(population_size: int, event_list: List[Event], dis
                               max_capacity: int = 100000, starting_ingredients: int = 500, visitors_coeff: float = 0.2,
                               distance_coeff: float = 50 / 100000, capacity_punishment_coeff: float = 18.0,
                               duration_punishment_coeff: float = 5000) -> List[Solution]:
-    population: List[Solution] = []
-    for _ in range(population_size):
+    population: List[Solution] = [None for _ in range(population_size)]
+    for i in range(population_size):
         solution_duration = 0
         solution_list = []
         while solution_duration < (end_date - start_date).days:
@@ -19,14 +20,49 @@ def create_initial_population(population_size: int, event_list: List[Event], dis
             solution_element = SolutionElement(event.event_id, stay_duration, ingredients_bought, **event.attributes)
             solution_list.append(solution_element)
 
-        population.append(
-            Solution(solution_list, distances, start_date, end_date, start_city, product_price, max_capacity,
-                     starting_ingredients, visitors_coeff, distance_coeff, capacity_punishment_coeff,
-                     duration_punishment_coeff))
+        population[i] = Solution(solution_list, distances, start_date, end_date, start_city, product_price,
+                                 max_capacity, starting_ingredients, visitors_coeff, distance_coeff,
+                                 capacity_punishment_coeff, duration_punishment_coeff)
     return population
 
 
-def selection(population: List[Solution], method: str = "rank") -> List[Solution]:
+def selection(population: List[Solution], parents_percent: float, method: str = "rank") -> List[Solution]:
     if method == 'rank':
-        population = sorted(population, key=lambda x: x.overall_profit(), reverse=True)
-        return population
+        num_of_parents = int(len(population) * parents_percent / 100)
+        if num_of_parents % 2 == 1:
+            num_of_parents += 1
+        population.sort(key=lambda x: x.overall_profit(), reverse=True)
+        parents = population[:num_of_parents]
+        return parents
+
+
+def crossover(parents: List[Solution], method="one_point") -> List[Solution]:
+    children: List[Union[None, Solution]] = [None for _ in range(len(parents))]
+    random.shuffle(parents)
+    if method == "one_point":
+        for i in range(0, len(parents), 2):
+            parent1 = deepcopy(parents[i])
+            parent2 = deepcopy(parents[i + 1])
+
+            crossover_point = random.randint(1, min(len(parent1.solution_list), len(parent2.solution_list)) - 1)
+            child1_solution_list = parent1.solution_list[:crossover_point] + parent2.solution_list[crossover_point:]
+            child2_solution_list = parent2.solution_list[:crossover_point] + parent1.solution_list[crossover_point:]
+            child1 = parent1
+            child1.solution_list = child1_solution_list
+            child2 = parent2
+            child2.solution_list = child2_solution_list
+
+            children[i] = child1
+            children[i + 1] = child2
+
+    return children
+
+
+def mutation(child: Solution, method="uniform"):
+    if method == "uniform":
+        index = random.randint(0, len(child.solution_list) - 1)
+        element = child.solution_list[index]
+        if random.random() > 0.5:
+            element.stay_duration = random.randint(1, (element.end_date - element.start_date).days + 1)
+        else:
+            element.ingredients_bought = random.randint(0, child.max_capacity)
--- main.py
+++ main.py
@@ -10,26 +10,28 @@ if __name__ == '__main__':
     event_list = load_event_list(df)
 
     # distances = driving_distances(list(df['city'].unique()))
+    # with open('distances.json', 'w') as fp:
+    #     json.dump(distances, fp)
     with open('distances.json', 'r') as fp:
         distances = json.load(fp)
 
     start_date = datetime(2022, 7, 4)
-    end_date = datetime(2022, 7, 10)
+    end_date = datetime(2022, 7, 15)
 
-    population = create_initial_population(100000, event_list, distances, start_date, end_date, 'Kraków')
-    population = selection(population)
-    print(population[0].overall_profit())
+    generations = 1000
+    best_solutions = []
+    population = create_initial_population(30, event_list, distances, start_date, end_date, 'Kraków')
+    for _ in range(generations):
+        parents = selection(population, 30)
+        best_solutions.append(population[0].overall_profit())
+        children = crossover(parents)
+        mutation_chance = 0.1
+        for child in children:
+            if random.random() <= 0.1:
+                mutation(child)
 
-    pass
+        population[-len(children):] = children
 
-    # solution_list = []
-    # solution_list.append(SolutionElement(0, 2, 6000, **event_list[0].attributes))
-    # solution_list.append(SolutionElement(1, 1, 2400, **event_list[1].attributes))
-    # solution_list.append(SolutionElement(2, 1, 1600, **event_list[2].attributes))
-    # solution_list.append(SolutionElement(3, 2, 5000, **event_list[3].attributes))
-    #
-    # solution = Solution(solution_list, distances, start_date, end_date, 'Kraków')
-    # for element in solution_list:
-    #     print(element)
-    #
-    # print(f"Całkowity zysk: {solution.overall_profit()}")
+    print(max(population, key=lambda x: x.overall_profit()).overall_profit())
+    print(best_solutions)
+
